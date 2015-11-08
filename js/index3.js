a3f398e5641d8e6d02d0c3b732917adad1c35ab7
stone-jin <jb1520006273@gmail.com>
2015-11-08 23:34:09 +0800

    今天完成，左侧的动态效果，还有navbar这块的内容

--- js/index3.js
+++ js/index3.js
@@ -4,5 +4,16 @@
 $(document).ready(function(){
 
    $('#side-menu').metisMenu();
+});
+
+$('.test_btn').click(function(){
+    $("body").toggleClass('mini-navbar');
+    if($("body").hasClass("mini-navbar")){
+        $(".left").animate({"width" : "50px"}, 300);
+        $(".right").animate({"margin-left": "50px"}, 300);
+    }else{
+        $(".left").animate({"width" : "220px"}, 300);
+        $(".right").animate({"margin-left": "220px"}, 300);
+    }
 
 });
